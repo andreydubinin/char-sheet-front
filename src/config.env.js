46cc062061699f232d22769cbaec26536489c420
Andrey Dubinin <ald91@mail.ru>
2021-05-16 17:41:37 +0700

    for render.com

---
+++ src/config.env.js
@@ -0,0 +1,3 @@
+// export const apiUrl = "http://37.193.182.4:8089/api/";
+export const apiUrl = "https://charsheet-api.duckdns.org/api/";
+export const tokenRefreshBefore = 60 * 30 * 1000; // 30min
